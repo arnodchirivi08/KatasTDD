3b5d535383b52bcaedbdc1ef066e2d038c862a22
Arnod Chiriví <arnod.chirivi@sinco.com.co>
2025-11-08 14:34:06 -0500

    🟥 Prueba para retornar el valor de los cepillos

---
+++ KatasACP/ReciboDeSupermercado/ReciboSupermercadoTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Katas.Stack;
+
+namespace Katas.ReciboDeSupermercado
+{
+    public class ReciboSupermercadoTest
+    {
+        [Fact]
+        public void Debe_CalcularCostoTotal_DevolveElvalorTotalDe2UnidadesDeCepillosPorElPrecioDeUnoQueSon_0_99_Euros()
+        {
+            //Arrange
+            var costoTotal = 0.99m;
+            var reciboSuper = new ReciboSupermercadoJ();
+            //Act
+
+
+            //Assert
+            reciboSuper.CalcularCostoTotal().Should().Be(costoTotal);
+        }
+    }
+
+    internal class ReciboSupermercadoJ
+    {
+        public ReciboSupermercadoJ()
+        {
+        }
+
+        public object CalcularCostoTotal()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
